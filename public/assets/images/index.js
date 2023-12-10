7be6caa48051a96c6bafa0300dbc2df6a7a9c0ae
droxy <chiomaotu22@gmail.com>
2023-12-10 11:41:45 +0100

    added images for organisers

--- public/assets/images/index.js
+++ public/assets/images/index.js
@@ -3,5 +3,23 @@ import greenRibbon from './green-ribbon.png';
 import holibells from './holiday-bell.png';
 import newsletterAlert from './newsletter.png';
 import secretGifterLogo from './secret-gifter-logo.png';
+import account1 from './account1.png';
+import account2 from './account-2.png';
+import event1 from './event1.png';
+import event2 from './event2.png';
+import invite1 from './invite1.png';
+import invite2 from './invite2.png';
 
-export { heroImage, greenRibbon, holibells, newsletterAlert, secretGifterLogo };
+export {
+  heroImage,
+  greenRibbon,
+  holibells,
+  newsletterAlert,
+  secretGifterLogo,
+  account1,
+  account2,
+  event1,
+  event2,
+  invite1,
+  invite2,
+};
