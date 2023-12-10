13a4d55eb6f09821af8d910f2a868e8e950fe7cf
droxy <chiomaotu22@gmail.com>
2023-12-10 14:03:21 +0100

    added images for participants

--- public/assets/images/index.js
+++ public/assets/images/index.js
@@ -10,6 +10,13 @@ import event2 from './event2.png';
 import invite1 from './invite1.png';
 import invite2 from './invite2.png';
 
+import choose1 from './choose1.png';
+import choose2 from './choose2.png';
+import wish1 from './replace-wish.png';
+import wish2 from './wish2.png';
+import mail1 from './mail1.png';
+import mail2 from './mail2.png';
+
 export {
   heroImage,
   greenRibbon,
@@ -22,4 +29,10 @@ export {
   event2,
   invite1,
   invite2,
+  choose1,
+  choose2,
+  wish1,
+  wish2,
+  mail1,
+  mail2,
 };
