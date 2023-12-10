0b1877fa00228a8fcf5d84fb307050b59f881c84
droxy <chiomaotu22@gmail.com>
2023-12-10 11:42:22 +0100

    added meta tags and data

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,43 @@ const dmSans = DM_Sans({ subsets: ['latin'] });
 import Layout from '@/components/layouts/Layout';
 import { Hero, HowItWorks, Newsletter } from '@/components/sections';
 
+import Head from 'next/head';
+
 const Home = () => {
   return (
     <Layout>
+      <Head>
+        <meta charset="utf-8" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+
+        <meta name="description" content="Secret Gifter" />
+
+        <meta
+          property="og:image"
+          content="%PUBLIC_URL%/assets/icons/logo.svg"
+        />
+        <meta property="og:title" content="Secret Gifter" />
+        <meta
+          property="og:description"
+          content="Add an element of surprise and joy to your social circle—because every occasion deserves a little Secret Gifter magic!"
+        />
+
+        <meta
+          property="og:image"
+          content="%PUBLIC_URL%/assets/icons/logo.svg"
+        />
+
+        <link
+          rel="apple-touch-icon"
+          href="%PUBLIC_URL%/assets/icons/logo.svg"
+        />
+
+        {/* <link rel="manifest" href="%PUBLIC_URL%/manifest.json" /> */}
+
+        <title>Secret Gifter</title>
+      </Head>
       <div className={`${dmSans.className}`}>
         <Hero />
         <HowItWorks />
