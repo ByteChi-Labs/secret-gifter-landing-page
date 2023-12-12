93bcfd70aed198deaf62e6dd66c20173545da352
Peter Okerulu <oduchep@gmail.com>
2023-12-12 13:39:00 +0100

    switched all blured images

--- public/assets/images/index.js
+++ public/assets/images/index.js
@@ -3,19 +3,19 @@ import greenRibbon from './green-ribbon.png';
 import holibells from './holiday-bell.png';
 import newsletterAlert from './newsletter.png';
 import secretGifterLogo from './secret-gifter-logo.png';
-import account1 from './account1.png';
-import account2 from './account-2.png';
-import event1 from './event1.png';
-import event2 from './event2.png';
-import invite1 from './invite1.png';
-import invite2 from './invite2.png';
+import account1 from './account1.svg';
+import account2 from './account2.svg';
+import event1 from './event1.svg';
+import event2 from './event2.svg';
+import invite1 from './invite1.svg';
+import invite2 from './invite2.svg';
 
-import choose1 from './choose1.png';
-import choose2 from './choose2.png';
-import wish1 from './replace-wish.png';
-import wish2 from './wish2.png';
-import mail1 from './mail1.png';
-import mail2 from './mail2.png';
+import choose1 from './choose1.svg';
+import choose2 from './choose2.svg';
+import wish1 from './wish1.svg';
+import wish2 from './wish2.svg';
+import mail1 from './mail1.svg';
+import mail2 from './mail2.svg';
 
 export {
   heroImage,
